267d8446a2c2485bec48ee10a56992cbf17b406b
jarandmoen <58026639+jarandmoen@users.noreply.github.com>
2023-11-09 10:23:40 +0100

    Update InfluxDB-Exporter.py

--- InfluxDB-Exporter.py
+++ InfluxDB-Exporter.py
@@ -19,9 +19,6 @@ Version: 1.0
 License:
 This script is released under the "Free for All" license, which is a permissive, non-copyleft open source license. It permits users to do anything they wish with the code, without limitations, and requires no attribution or inclusion of the license itself. This means you may use, modify, distribute, and sublicense this code however you like, for commercial or non-commercial purposes, without restriction.
 
-Note:
-This license is not recognized by the Open Source Initiative (OSI) or the Free Software Foundation (FSF). It's intended for educational purposes and small personal projects. It's always recommended to consult with a legal expert or to use OSI-approved licenses for larger projects or those involving distribution.
-
 This script is provided "as is", without warranty of any kind, express or implied. In no event shall the authors or copyright holders be liable for any claim, damages, or other liability arising from the use of the software.
 """
 
